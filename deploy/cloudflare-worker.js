6e09d556ce8adcdbcbce3ab8307f8406f0f21408
Fish <Fish@coss.com.tw>
2026-07-16 16:19:22 +0800

    deploy: publish Stage 1 upgrade workbench

--- deploy/cloudflare-worker.js
+++ deploy/cloudflare-worker.js
@@ -43,7 +43,7 @@ export default {
     const headers = new Headers(upstream.headers);
     headers.set("Content-Type", contentTypeFor(pathname));
     headers.set("X-Content-Type-Options", "nosniff");
-    headers.set("X-PVZ-Release", "2026-07-16.2");
+    headers.set("X-PVZ-Release", "2026-07-16.3");
     headers.set(
       "Cache-Control",
       pathname.endsWith(".html") || pathname.endsWith(".pck")
